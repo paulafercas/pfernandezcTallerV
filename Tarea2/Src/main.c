7450faad914c189c745df591ee7f033e5b4ff4f3
Paula Andrea Fernandez <pfernandezc@unal.edu.co>
2025-05-27 12:48:36 -0500

    corrección de errores y warnings

--- Tarea2/Src/main.c
+++ Tarea2/Src/main.c
@@ -28,9 +28,6 @@ typedef enum{
 }parteNumero;
 
 
-
-void maquinaEstados(Estado actual, uint8_t digito);
-
 //Creamos la variable donde vamos a guardar el numero que se va a mostrar en el display
 uint16_t numeroDisplay =0;
 
@@ -61,14 +58,25 @@ GPIO_Handler_t alimentacion1={0}; //PinA12
 GPIO_Handler_t alimentacion2={0}; //PinB12
 GPIO_Handler_t alimentacion3={0}; //PinA11
 
-
+//Definimos los timers
 Timer_Handler_t blinkTimer = {0};
+Timer_Handler_t refreshTimer ={0};
 
 /*
  * Declaramos la funciones que aparecerán próximamente
  */
+//Funcion que define qué se va a hacer según el estado en el que se encuentre la maquina
+void maquinaEstados(Estado actual,uint8_t digito,uint8_t unidad, uint8_t decena, uint8_t centena, uint8_t milUnidad);
+//Funcion que separa el numeroDisplay en 4 partes (unidad, decena, centena, milUnidad)
 int separacion_parte (parteNumero parte, uint16_t numeroDisplay);
-
+//Funcion que nos asegura de que el parametro "digito" no vaya a ser mayor que 3,
+//ya que solo tenemos 4 digitos enumerados desde 0 a 3
+uint8_t valor_digito (uint8_t digito);
+//Funcion que nos permite encender el numero que queremos en el digito que queremos
+void digito_encendido(uint8_t digito, uint8_t unidad,uint8_t decena, uint8_t centena, uint8_t milUnidad);
+//Funcion que se encuentra dentro de "digito_encendido" para definir que numero
+//quiero ver en cada digito
+void definir_numero (uint8_t numero);
 
 /*
  * The main function, where everything happens.
@@ -190,23 +198,31 @@ int main(void)
 	blinkTimer.TIMx_Config.TIMx_mode				= TIMER_UP_COUNTER;
 	blinkTimer.TIMx_Config.TIMx_InterruptEnable 	= TIMER_INT_ENABLE;
 
-	blinkTimer.pTIMx								=TIM3;
-	blinkTimer.TIMx_Config.TIMx_Prescaler			=16000; //Genera incrementos de 1 ms
-	blinkTimer.TIMx_Config.TIMx_Period				=20;  // De la mano con el prescaler,
-	blinkTimer.TIMx_Config.TIMx_mode				= TIMER_UP_COUNTER;
-	blinkTimer.TIMx_Config.TIMx_InterruptEnable 	= TIMER_INT_ENABLE;
+	refreshTimer.pTIMx								=TIM3;
+	refreshTimer.TIMx_Config.TIMx_Prescaler			=16000; //Genera incrementos de 1 ms
+	refreshTimer.TIMx_Config.TIMx_Period				=20;  // De la mano con el prescaler,
+	refreshTimer.TIMx_Config.TIMx_mode				= TIMER_UP_COUNTER;
+	refreshTimer.TIMx_Config.TIMx_InterruptEnable 	= TIMER_INT_ENABLE;
 
-	/* Configuramos el Timer */
+	/* Configuramos el BlinkTimer */
 	timer_Config(&blinkTimer);
 
-	// Encendemos el Timer.
+	// Encendemos el BlinkTimer.
 	timer_SetState(&blinkTimer, TIMER_ON);
+
+	/* Configuramos el Timer */
+	timer_Config(&refreshTimer);
+
+	// Encendemos el Timer.
+	timer_SetState(&refreshTimer, TIMER_ON);
+
 
 	//Definimos la variable que va a gurdar el estado en el que se encuentra mi maquina de estados
 
-	Estado actual = actual;
 
-	maquinaEstados(actual,digito,unidad, decena, centena, milUnidad);
+	maquinaEstados(refrescar,digito,unidad, decena, centena, milUnidad);
+
+
 
     /* Loop forever */
 	while(1){
@@ -252,25 +268,30 @@ int separacion_parte (parteNumero parte, uint16_t numeroDisplay){
 		return milUnidad;
 	}
 	else {
-		__NOP();
+		return 0;
 	}
+}
 
-int maquinaEstados(Estado actual,uint8_t digito,uint8_t unidad, uint8_t decena, uint8_t centena, uint8_t milUnidad){
+void maquinaEstados(Estado actual,uint8_t digito,uint8_t unidad, uint8_t decena, uint8_t centena, uint8_t milUnidad){
+
 	if (actual ==refrescar){
 
 		//Primero debemos cersiorarnos de que el digito que queremos encender no tenga un
-		//valor mayor a 3 (ya que solo tenemos 4 digitos)
-		if (digito ==4){
-			digito == 0;
+		//valor mayor a 3 (ya que solo tenemos 4 digitos
+		digito = valor_digito(digito);
+		//Llamamos a la funcion que nos indica qué pines deben estar encendidos en el digito
+		//que deseo mostrar
+		digito_encendido(digito,unidad, decena, centena, milUnidad);
+
+	}
+}
+uint8_t valor_digito (uint8_t digito){
+	if (digito==4){
+			digito -= 4;
 		}
 		else{
 			__NOP();
 		}
-		digito_encendido(digito,unidad, decena, centena, milUnidad);
-
-		//Llamamos a la funcion que nos indica qué pines deben estar encendidos en el digito
-		//que deseo mostrar
-	}
 	return digito;
 }
 
@@ -278,26 +299,38 @@ void digito_encendido(uint8_t digito, uint8_t unidad,uint8_t decena, uint8_t cen
 	switch (digito){
 	//Caso en el cual queremos ver el numero en el digito de las unidades
 	case 0: {
+		//Apagamos el digito que estaba encendido anteriormente (en este caso el 4)
 		gpio_WritePin (&alimentacion3, SET);
+		//Encendemos los pines correspondientes al numero que se desea
 		definir_numero (unidad);
+		//Encedemos el digito (1) donde se va a mostrar el numero seleccionado anteriormente
 		gpio_WritePin (&alimentacion0, RESET);
 		break;
 	}
 	case 1: {
+		//Apagamos el digito que estaba encendido anteriormente (en este caso el 1)
 		gpio_WritePin (&alimentacion0, SET);
+		//Encendemos los pines correspondientes al numero que se desea
 		definir_numero (decena);
+		//Encedemos el digito (1) donde se va a mostrar el numero seleccionado anteriormente
 		gpio_WritePin (&alimentacion1, RESET);
 		break;
 	}
 	case 2:{
+		//Apagamos el digito que estaba encendido anteriormente (en este caso el 2)
 		gpio_WritePin (&alimentacion1, SET);
+		//Encendemos los pines correspondientes al numero que se desea
 		definir_numero (centena);
+		//Encedemos el digito (1) donde se va a mostrar el numero seleccionado anteriormente
 		gpio_WritePin (&alimentacion2, RESET);
 		break;
 	}
 	case 3:{
+		//Apagamos el digito que estaba encendido anteriormente (en este caso el 3)
 		gpio_WritePin (&alimentacion2, SET);
+		//Encendemos los pines correspondientes al numero que se desea
 		definir_numero (milUnidad);
+		//Encedemos el digito (1) donde se va a mostrar el numero seleccionado anteriormente
 		gpio_WritePin (&alimentacion3, RESET);
 		break;
 	}
@@ -308,69 +341,93 @@ void digito_encendido(uint8_t digito, uint8_t unidad,uint8_t decena, uint8_t cen
 	}// Fin del Switch-case
 }
 void definir_numero (uint8_t numero){
-	if (numero == 0){
-
+	switch (numero){
+	case 0: {
 		gpio_WritePin(&segmento1, RESET);
 		gpio_WritePin(&segmento2, RESET);
 		gpio_WritePin(&segmento4, RESET);
 		gpio_WritePin(&segmento7, RESET);
 		gpio_WritePin(&segmento10, RESET);
 		gpio_WritePin(&segmento11, RESET);
-	} else if (numero == 1){
-		gpio_WritePin(&segmento4, RESET);
-		gpio_WritePin(&segmento7, RESET);
-	} else if (numero ==2){
-		gpio_WritePin(&segmento1, RESET);
-		gpio_WritePin(&segmento2, RESET);
-		gpio_WritePin(&segmento5, RESET);
-		gpio_WritePin(&segmento7, RESET);
-		gpio_WritePin(&segmento11, RESET);
-	} else if (numero==3){
-		gpio_WritePin(&segmento2, RESET);
-		gpio_WritePin(&segmento5, RESET);
-		gpio_WritePin(&segmento4, RESET);
-		gpio_WritePin(&segmento7, RESET);
-		gpio_WritePin(&segmento11, RESET);
-	} else if (numero == 4){
-		gpio_WritePin(&segmento4, RESET);
-		gpio_WritePin(&segmento7, RESET);
-		gpio_WritePin(&segmento5, RESET);
-		gpio_WritePin(&segmento10, RESET);
-	} else if (numero == 5){
-		gpio_WritePin(&segmento2, RESET);
-		gpio_WritePin(&segmento4, RESET);
-		gpio_WritePin(&segmento5, RESET);
-		gpio_WritePin(&segmento10, RESET);
-		gpio_WritePin(&segmento11, RESET);
-	} else if (numero == 6){
-		gpio_WritePin(&segmento2, RESET);
-		gpio_WritePin(&segmento4, RESET);
-		gpio_WritePin(&segmento5, RESET);
-		gpio_WritePin(&segmento10, RESET);
-		gpio_WritePin(&segmento11, RESET);
-		gpio_WritePin(&segmento1, RESET);
-	} else if (numero == 7){
-		gpio_WritePin(&segmento4, RESET);
-		gpio_WritePin(&segmento7, RESET);
-		gpio_WritePin(&segmento11, RESET);
-	} else if (numero ==8){
-		gpio_WritePin(&segmento2, RESET);
-		gpio_WritePin(&segmento4, RESET);
-		gpio_WritePin(&segmento5, RESET);
-		gpio_WritePin(&segmento10, RESET);
-		gpio_WritePin(&segmento11, RESET);
-		gpio_WritePin(&segmento1, RESET);
-		gpio_WritePin(&segmento7, RESET);
-	} else if (numero ==9){
-		gpio_WritePin(&segmento4, RESET);
-		gpio_WritePin(&segmento5, RESET);
-		gpio_WritePin(&segmento10, RESET);
-		gpio_WritePin(&segmento11, RESET);
-		gpio_WritePin(&segmento7, RESET);
-	} else {
-		__NOP();
+		break;
 	}
+	case 1: {
+		gpio_WritePin(&segmento4, RESET);
+		gpio_WritePin(&segmento7, RESET);
+		break;
+	}
+	case 2: {
+		gpio_WritePin(&segmento1, RESET);
+		gpio_WritePin(&segmento2, RESET);
+		gpio_WritePin(&segmento5, RESET);
+		gpio_WritePin(&segmento7, RESET);
+		gpio_WritePin(&segmento11, RESET);
+		break;
+	}
+	case 3: {
+		gpio_WritePin(&segmento2, RESET);
+		gpio_WritePin(&segmento5, RESET);
+		gpio_WritePin(&segmento4, RESET);
+		gpio_WritePin(&segmento7, RESET);
+		gpio_WritePin(&segmento11, RESET);
+		break;
+	}
+	case 4: {
+		gpio_WritePin(&segmento4, RESET);
+		gpio_WritePin(&segmento7, RESET);
+		gpio_WritePin(&segmento5, RESET);
+		gpio_WritePin(&segmento10, RESET);
+		break;
+	}
+	case 5: {
+		gpio_WritePin(&segmento2, RESET);
+		gpio_WritePin(&segmento4, RESET);
+		gpio_WritePin(&segmento5, RESET);
+		gpio_WritePin(&segmento10, RESET);
+		gpio_WritePin(&segmento11, RESET);
+		break;
+	}
+	case 6: {
+		gpio_WritePin(&segmento2, RESET);
+		gpio_WritePin(&segmento4, RESET);
+		gpio_WritePin(&segmento5, RESET);
+		gpio_WritePin(&segmento10, RESET);
+		gpio_WritePin(&segmento11, RESET);
+		gpio_WritePin(&segmento1, RESET);
+		break;
+	}
+	case 7: {
+		gpio_WritePin(&segmento4, RESET);
+		gpio_WritePin(&segmento7, RESET);
+		gpio_WritePin(&segmento11, RESET);
+		break;
+	}
+	case 8: {
+		gpio_WritePin(&segmento2, RESET);
+		gpio_WritePin(&segmento4, RESET);
+		gpio_WritePin(&segmento5, RESET);
+		gpio_WritePin(&segmento10, RESET);
+		gpio_WritePin(&segmento11, RESET);
+		gpio_WritePin(&segmento1, RESET);
+		gpio_WritePin(&segmento7, RESET);
+		break;
+	}
+	case 9: {
+		gpio_WritePin(&segmento4, RESET);
+		gpio_WritePin(&segmento5, RESET);
+		gpio_WritePin(&segmento10, RESET);
+		gpio_WritePin(&segmento11, RESET);
+		gpio_WritePin(&segmento7, RESET);
+		break;
+	}
+	default: {
+		__NOP();
+		break;
+	}
+	}// Fin del switch-case
+
 }
+
 void Timer2_Callback(void){
 	gpio_TooglePin(&userLed);
 }
@@ -387,6 +444,7 @@ void assert_failed(uint8_t* file, uint32_t line){
 		// problems...
 	}
 }
+
 
 
 
